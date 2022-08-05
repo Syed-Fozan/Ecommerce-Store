e4224d922663c64c30d1b8a3794d93b2eefefe4f
Syed Fozan Asif <asiffozan903@gmail.com>
2022-08-05 12:41:09 +0500

    header footer and login page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+  
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2022 Copyright:
+      <a href="/"> Ecommerce Store</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
